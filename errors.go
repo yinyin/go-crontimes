9fd753f73d87b41dc9360163d91ab8ba7095540d
Yinyin L <yinyinl@gmail.com>
2020-02-29 00:50:51 +0800

    define errors for expression parsing

---
+++ errors.go
@@ -0,0 +1,45 @@
+package crontimes
+
+import (
+	"strconv"
+)
+
+//go:generate stringer -type ExpressionErrType errors.go
+
+// ExpressionErrType defines type of errors.
+type ExpressionErrType int
+
+// Identifiers of error types.
+const (
+	noneExpressionErr ExpressionErrType = iota
+	ErrSyntaxValueAfterLastRule
+	ErrSyntaxUnexpectCharacter
+	ErrSyntaxUnexpectLocation
+	ErrStepMustNotBeZero
+	ErrValueOutOfValidRange
+	ErrLastNotSupportInMinuteRule
+	ErrWeekdayNotSupportInMinuteRule
+	ErrLastNotSupportInHourRule
+	ErrWeekdayNotSupportInHourRule
+	ErrLastDayOfMonthSyntax
+	ErrLastNotSupportInMonthRule
+	ErrWeekdayNotSupportInMonthRule
+	ErrWeekdayNotSupportInDayOfWeekRule
+)
+
+// ErrCronExpression represent errors of cron expressions
+type ErrCronExpression struct {
+	ErrType   ExpressionErrType
+	CharIndex int
+}
+
+func newErrCronExpression(errType ExpressionErrType, charIndex int) (e *ErrCronExpression) {
+	return &ErrCronExpression{
+		ErrType:   errType,
+		CharIndex: charIndex,
+	}
+}
+
+func (e *ErrCronExpression) Error() string {
+	return "[CronExpressionError: " + e.ErrType.String() + " (" + strconv.FormatInt(int64(e.ErrType), 10) + "), Index=" + strconv.FormatInt(int64(e.CharIndex), 10) + "]"
+}
